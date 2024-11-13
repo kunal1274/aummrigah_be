d9efcf6b5891b8a23adedf3b7f5577efe6cf67e9
kunal1274 <kunal1274@gmail.com>
2024-11-13 22:06:37 +0530

    cors and mongo db connection testing

--- database/mongodb.js
+++ database/mongodb.js
@@ -5,7 +5,14 @@ import ce from "../utility/ce.js";
 const connectToDb = async () => {
   const uriWithEnv = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.PROJECT_NAME}.pnctyau.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority&appName=${process.env.APP_NAME}`;
   try {
-    const { connection } = await mongoose.connect(uriWithEnv);
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true, // Ensures Mongoose uses the new connection engine
+      serverSelectionTimeoutMS: 5000, // Reduces connection timeout for faster error reporting
+      socketTimeoutMS: 45000, // Keeps the connection alive for up to 45 seconds
+    };
+
+    const { connection } = await mongoose.connect(uriWithEnv, options);
     if (connection) {
       cl({
         message: `Server is connected to database successfully at ${connection.host}`,
